3ff4eaeb1bfd1a07169879512423251c008725cd
yongjunlee <chesidyong@gmail.com>
2020-03-16 23:52:27 +0900

    Add login-decorator

--- user/tests.py
+++ user/tests.py
@@ -37,3 +37,38 @@ class NaverSignInTest(TestCase):
                 "user" : {"nickname": "test_nickname", "image": "test_image"}
             }
         )
+
+class LoginRequiredTest(TestCase):
+    @patch('user.views.requests')
+    def test_user_signin_success(self, mocked_request):
+        class NaverResponse:
+            def json(self):
+                return {
+                    "resultcode"        : "00",
+                    "message"           : "success",
+                    "response"          :
+                    {
+                        "email"         : "test@email.com",
+                        "nickname"      : "test_nickname",
+                        "profile_image" : "test_image",
+                        "age"           : "20-29",
+                        "gender"        : "F",
+                        "id"            : "12345678",
+                        "name"          : "test_name",
+                        "birthday"      : "1910-11-00"
+                    }
+                }
+        mocked_request.get = MagicMock(return_value = NaverResponse())
+
+        client = Client()
+        header = {'HTTP_Authorization': 'naver_token'}
+        response = client.get('/user/naver_auth', content_type = 'applications/json', **header)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTIzNDU2NzgifQ.qsllxE7wljfDv0QjgmsuOU5vWnIe_g9W7bgYyGngg_M",
+                "user" : {"nickname": "test_nickname", "image": "test_image"}
+            }
+        )
+
